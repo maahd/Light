f4ca2cee92f76795075946727b412b7c66f6689f
maahd <maahdsh@gmail.com>
2019-03-16 23:45:32 +0100

    UX improvements

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ViewController: UIViewController {
     
     var lightOn = true
-    @IBOutlet weak var lightButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +23,7 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
-        if lightOn {
-            view.backgroundColor = .white
-            lightButton.setTitle("Off", for: .normal)
-        } else {
-            view.backgroundColor = .black
-            lightButton.setTitle("On", for: .normal)
-        }
+        view.backgroundColor = lightOn ? .white : .black
     }
 }
 
