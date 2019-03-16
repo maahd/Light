32112096f10152a5a4925d0d0462dd701604e64a
maahd <maahdsh@gmail.com>
2019-03-16 23:29:13 +0100

    Torch working

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var lightOn = true
+    @IBOutlet weak var lightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    
+    func updateUI() {
+        if lightOn {
+            view.backgroundColor = .white
+            lightButton.setTitle("Off", for: .normal)
+        } else {
+            view.backgroundColor = .black
+            lightButton.setTitle("On", for: .normal)
+        }
+    }
 }
 
